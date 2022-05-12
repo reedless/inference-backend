f345b8bc9ab3ad6e34cf2fd00f1213bf37570435
Howard <howardyanghaowen@gmail.com>
2022-05-12 13:32:06 +0800

    added absolute paths

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
-python3 -m venv .env && \
-source .env/bin/activate && \
-pip3 install -r requirements.txt && \
+python3 -m venv ~/.env && \
+source ~/.env/bin/activate && \
+pip3 install -r ~/inference-backend/requirements.txt && \
 pip3 install torch==1.10.2 && \
-python3 inference_backend.py
+python3 ~/inference-backend/inference_backend.py
