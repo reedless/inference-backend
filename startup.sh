3fcb061eca5eceb6fd5903d3029b2c4c1c584634
Howard <howardyanghaowen@gmail.com>
2022-05-12 13:23:38 +0800

    added torch 1.10.2 instead

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 python3 -m venv .env && \
 source .env/bin/activate && \
 pip3 install -r requirements.txt && \
-pip3 install torch==1.9.1+cpu -f https://download.pytorch.org/whl/torch_stable.html && \
+pip3 install torch==1.10.2 && \
 python3 inference_backend.py
